801adf688d48d2aa4afbfc0db778fce0f6f7c78b
Natalia Salazar <nataliasalazardo@gmail.com>
2026-06-15 11:47:52 -0600

    codigo comentado

--- leds.c
+++ leds.c
@@ -6,10 +6,15 @@
 
 #include "driver/gpio.h"
 
+/* Tarea encargada de controlar un LED individual.
+   Cada LED recibe sus parámetros mediante pvParameters. */
 void led_task(void *pvParameters)
 {
+    /* Se convierte el parámetro genérico recibido a la estructura
+       específica de configuración del LED. */
     LedTaskParams_t *cfg = (LedTaskParams_t *)pvParameters;
 
+    /* Configura el GPIO asignado al LED como salida digital. */
     gpio_reset_pin((gpio_num_t)cfg->gpio);
     gpio_set_direction((gpio_num_t)cfg->gpio, GPIO_MODE_OUTPUT);
     gpio_set_level((gpio_num_t)cfg->gpio, 0);
@@ -18,20 +23,19 @@ void led_task(void *pvParameters)
     {
         uint8_t bit_value;
 
-		
+        /* Extrae el bit correspondiente del valor global del contador.
+           Cada LED representa un bit del número BCD. */
         bit_value = (g_system.value >> cfg->bit_position) & 0x01;
 
-		/*TODO --> Después de actualizar el valor de bit_value, enviar a gpio*/
-		
+        /* Envía el valor del bit al GPIO del LED:
+           1 enciende el LED, 0 lo apaga. */
         gpio_set_level(
-        (gpio_num_t)cfg->gpio,
-        bit_value
+            (gpio_num_t)cfg->gpio,
+            bit_value
         );
 
-        /*
-           Tarea ligera: actualiza LED periódicamente.
-           Cuando no hay cambios, queda BLOCKED.
-        */
+        /* Pequeño retardo para actualizar el LED periódicamente
+           sin consumir el CPU constantemente. */
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
